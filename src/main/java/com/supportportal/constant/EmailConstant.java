7b568f8e8f4f7af00d1e56549098e6e11b7b536f
Amine_Lab <99009160+amine58920901@users.noreply.github.com>
2022-08-30 13:55:58 +0100

    Update EmailConstant.java

--- src/main/java/com/supportportal/constant/EmailConstant.java
+++ src/main/java/com/supportportal/constant/EmailConstant.java
@@ -2,8 +2,8 @@ package com.supportportal.constant;
 
 public class EmailConstant {
     public static final String SIMPLE_MAIL_TRANSFER_PROTOCOL = "smtps";
-    public static final String USERNAME = "labiodh.amine90@gmail.com";
-    public static final String PASSWORD = "ayqcnohjfiprvlud";
+    public static final String USERNAME = "email";
+    public static final String PASSWORD = "";
     public static final String FROM_EMAIL = "support@getarrays.com";
     public static final String CC_EMAIL = "";
     public static final String EMAIL_SUBJECT = "Maxi-Log, LLC - New Password";
